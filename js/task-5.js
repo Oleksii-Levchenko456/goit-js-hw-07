a3d1946a3bdf94e93f2b984aad6a3f5337a87ca4
Oleksii <neonator3000@gmail.com>
2025-07-10 18:36:30 +0200

    task 5 ready

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color')
+
+const color = document.querySelector('.color')
+
+
+button.addEventListener('click', () => {
+  const newColor = getRandomHexColor()
+  document.body.style.backgroundColor = 
+  color.textContent = newColor
+})
+
